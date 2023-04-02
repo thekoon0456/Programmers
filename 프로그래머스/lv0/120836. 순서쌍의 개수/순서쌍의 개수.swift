e32fcbfa77541a21a64bd350a679e6a308def77d
thekoon <thekoon0456@gmail.com>
2023-04-02 14:49:21 +0900

    [level 0] Title: 순서쌍의 개수, Time: 452.85 ms, Memory: 16.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120836. 순서쌍의 개수/순서쌍의 개수.swift
+++ 프로그래머스/lv0/120836. 순서쌍의 개수/순서쌍의 개수.swift
@@ -1,14 +1,5 @@
 import Foundation
 
 func solution(_ n:Int) -> Int {
-    var count = 0
-    
-    for i in 1...n {
-        
-        if n % i == 0 {
-            count += 1
-        }
-        
-    }
-    return count
+    return (1...n).filter { n % $0 == 0 }.count
 }
