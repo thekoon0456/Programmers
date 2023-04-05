2994ab87b8a626a57070fad639e926e75cd8d888
thekoon <thekoon0456@gmail.com>
2023-04-05 18:23:53 +0900

    [level 0] Title: A로 B 만들기, Time: 0.09 ms, Memory: 16.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120886. A로 B 만들기/A로 B 만들기.swift
+++ 프로그래머스/lv0/120886. A로 B 만들기/A로 B 만들기.swift
@@ -1,16 +1,5 @@
 import Foundation
 
 func solution(_ before:String, _ after:String) -> Int {
-    var beforeNums = before.map { $0 }
-    var afterNums = after.map { $0 }
-    
-    for i in beforeNums {
-        if afterNums.contains(i) {
-            if let num = afterNums.firstIndex(of: i) {
-                afterNums.remove(at:num)
-            }
-        }
-    }
-    
-    return afterNums.isEmpty ? 1 : 0
+    return before.sorted() == after.sorted() ? 1 : 0
 }
