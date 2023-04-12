7f86e434fc08e5ad76890d9c49cc23658652d0bc
thekoon <thekoon0456@gmail.com>
2023-04-13 00:27:13 +0900

    [level 2] Title: 최솟값 만들기, Time: 5.66 ms, Memory: 16.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12941. 최솟값 만들기/최솟값 만들기.swift
@@ -0,0 +1,15 @@
+import Foundation
+
+func solution(_ A:[Int], _ B:[Int]) -> Int {
+    var arrA = A.sorted()
+    var arrB = B.sorted()
+    var result = 0
+    
+    for _ in 0..<arrA.count {
+        result += arrA.first! * arrB.last!
+            arrA.removeFirst()
+            arrB.removeLast()
+    }
+    
+    return result
+}
