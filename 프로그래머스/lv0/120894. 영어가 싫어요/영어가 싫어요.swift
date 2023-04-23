7b6d586dda77054af06483b7ee0972ca33f81a17
thekoon <thekoon0456@gmail.com>
2023-04-24 02:40:13 +0900

    [level 0] Title: 영어가 싫어요, Time: 0.09 ms, Memory: 16.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120894. 영어가 싫어요/영어가 싫어요.swift
+++ 프로그래머스/lv0/120894. 영어가 싫어요/영어가 싫어요.swift
@@ -1,14 +1,19 @@
 import Foundation
 
 func solution(_ numbers:String) -> Int64 {
-    var numbers = numbers
     var dicNumbers = ["zero" : 0, "one" : 1, "two" : 2, "three" : 3, "four" : 4, "five" : 5, "six" : 6, "seven" : 7, "eight" : 8, "nine" : 9]
     
-    for (key, value) in dicNumbers {
-        if numbers.contains(key) {
-            numbers = numbers.replacingOccurrences(of: key, with: String(value))
+    var result = ""
+    var temp: String = ""
+    
+    for char in numbers {
+        temp += String(char)
+        
+        if dicNumbers.keys.contains(temp) {
+            result += String(dicNumbers[temp]!)
+            temp = ""
         }
     }
     
-    return Int64(numbers)!
+    return Int64(result)!
 }
