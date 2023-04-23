19e547c5ed4261885598ef8c11586a0cc00f8fd6
thekoon <thekoon0456@gmail.com>
2023-04-24 00:49:06 +0900

    [unrated] Title: 문자열의 앞의 n글자, Time: 0.01 ms, Memory: 16.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181907. 문자열의 앞의 n글자/문자열의 앞의 n글자.swift
+++ 프로그래머스/unrated/181907. 문자열의 앞의 n글자/문자열의 앞의 n글자.swift
@@ -1,7 +1,5 @@
 import Foundation
 
 func solution(_ my_string:String, _ n:Int) -> String {
-    var myString = my_string.map { String($0) }
-    myString.removeSubrange(n..<myString.count)
-    return myString.reduce("", +)
+    String(my_string.prefix(n))
 }
