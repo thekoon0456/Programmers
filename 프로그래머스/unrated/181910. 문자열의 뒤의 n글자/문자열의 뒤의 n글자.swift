5863a3b74e9698474e41181c7fa85436c9546038
thekoon <thekoon0456@gmail.com>
2023-05-16 10:44:13 +0900

    [unrated] Title: 문자열의 뒤의 n글자, Time: 0.01 ms, Memory: 16.2 MB -BaekjoonHub

--- 프로그래머스/unrated/181910. 문자열의 뒤의 n글자/문자열의 뒤의 n글자.swift
+++ 프로그래머스/unrated/181910. 문자열의 뒤의 n글자/문자열의 뒤의 n글자.swift
@@ -1,11 +1,5 @@
 import Foundation
 
 func solution(_ my_string:String, _ n:Int) -> String {
-    var myString = my_string
-    
-    for _ in 0..<myString.count - n {
-        myString = String(myString.dropFirst())
-    }
-    
-    return myString
+    return String(my_string.suffix(n))
 }
