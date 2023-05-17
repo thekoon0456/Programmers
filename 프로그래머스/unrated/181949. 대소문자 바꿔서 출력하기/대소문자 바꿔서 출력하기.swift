7786ae7f572f052297396795a155fb117729fa53
thekoon <thekoon0456@gmail.com>
2023-05-17 16:07:54 +0900

    [unrated] Title: 대소문자 바꿔서 출력하기, Time: 4.91 ms, Memory: 12.8 MB -BaekjoonHub

--- 프로그래머스/unrated/181949. 대소문자 바꿔서 출력하기/대소문자 바꿔서 출력하기.swift
+++ 프로그래머스/unrated/181949. 대소문자 바꿔서 출력하기/대소문자 바꿔서 출력하기.swift
@@ -1,27 +1,11 @@
 import Foundation
 
 let s1 = readLine()!
-
 var result: String = ""
 
 for str in s1 {
-    if str.isLowercased {
-        result += str.uppercased()
-    } else {
-        result += str.lowercased()
-    }
+    result += String(str.isLowercase ? str.uppercased() : str.lowercased())
 }
 
 print(result)
 
-extension Character {
-    var isLowercased: Bool {
-        for c in utf8 where (65...90) ~= c { return false }
-        return true
-    }
-
-    var isUppercased: Bool {
-        for c in utf8 where (97...122) ~= c { return false }
-        return true
-    }
-}
