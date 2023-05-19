2e8b5b4a9d6f58b8a65458e176c4c9be3b3d3104
thekoon <thekoon0456@gmail.com>
2023-05-19 17:51:45 +0900

    [unrated] Title: 첫 번째로 나오는 음수, Time: 0.01 ms, Memory: 16.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181896. 첫 번째로 나오는 음수/첫 번째로 나오는 음수.swift
+++ 프로그래머스/unrated/181896. 첫 번째로 나오는 음수/첫 번째로 나오는 음수.swift
@@ -1,14 +1,5 @@
 import Foundation
 
 func solution(_ num_list:[Int]) -> Int {
-    var result = -1
-    
-    for i in 0..<num_list.count {
-        if num_list[i] < 0 {
-            result = i
-            break
-        }
-    }
-    
-    return result
+    num_list.firstIndex(where: { $0 < 0 }) ?? -1
 }
