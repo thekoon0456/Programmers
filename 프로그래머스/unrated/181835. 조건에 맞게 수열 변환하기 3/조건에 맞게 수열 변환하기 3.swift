a8e0936a722b8fdb9e0814315679140086e56079
thekoon <thekoon0456@gmail.com>
2023-05-20 22:30:45 +0900

    [unrated] Title: 조건에 맞게 수열 변환하기 3, Time: 10.91 ms, Memory: 38.2 MB -BaekjoonHub

--- 프로그래머스/unrated/181835. 조건에 맞게 수열 변환하기 3/조건에 맞게 수열 변환하기 3.swift
+++ 프로그래머스/unrated/181835. 조건에 맞게 수열 변환하기 3/조건에 맞게 수열 변환하기 3.swift
@@ -1,11 +1,5 @@
 import Foundation
 
 func solution(_ arr:[Int], _ k:Int) -> [Int] {
-    var result = [Int]()
-    
-    for i in arr {
-         result.append(k % 2 != 0 ? i * k : i + k) 
-    }
-    
-    return result
+    arr.map { k % 2 != 0 ? $0 * k : $0 + k }
 }
