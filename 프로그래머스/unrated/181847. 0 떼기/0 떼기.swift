45dfb864a40dc8d5312c41418375f50a916299cb
thekoon <thekoon0456@gmail.com>
2023-05-25 14:15:48 +0900

    [unrated] Title: 0 떼기, Time: 0.03 ms, Memory: 16.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181847. 0 떼기/0 떼기.swift
+++ 프로그래머스/unrated/181847. 0 떼기/0 떼기.swift
@@ -1,15 +1,5 @@
 import Foundation
 
 func solution(_ n_str:String) -> String {
-    var result = n_str
-    
-    for i in n_str {
-        if i == "0" {
-            result.removeFirst() 
-        } else {
-            break
-        }
-    }
-    
-    return result
+    String(Int(n_str) ?? 0)
 }
