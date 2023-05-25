31de63ea0fbee75d68e0971001982958b8bed0e4
thekoon <thekoon0456@gmail.com>
2023-05-25 13:53:45 +0900

    [unrated] Title: rny_string, Time: 0.11 ms, Memory: 16.2 MB -BaekjoonHub

--- 프로그래머스/unrated/181863. rny＿string/rny＿string.swift
+++ 프로그래머스/unrated/181863. rny＿string/rny＿string.swift
@@ -1,5 +1,5 @@
 import Foundation
 
 func solution(_ rny_string:String) -> String {
-    rny_string.map { String($0) == "m" ? "rn" : String($0) }.reduce("", +)
+    rny_string.replacingOccurrences(of: "m", with: "rn")
 }
