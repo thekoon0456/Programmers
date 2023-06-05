5d8d3877c0fed8f66c8868a4130288bcbdf9413e
thekoon <thekoon0456@gmail.com>
2023-06-05 12:50:22 +0900

    [unrated] Title: n개 간격의 원소들, Time: 0.08 ms, Memory: 16.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181888. n개 간격의 원소들/n개 간격의 원소들.swift
+++ 프로그래머스/unrated/181888. n개 간격의 원소들/n개 간격의 원소들.swift
@@ -1,13 +1,5 @@
 import Foundation
 
 func solution(_ num_list:[Int], _ n:Int) -> [Int] {
-    var result = [Int]()
-    
-    for i in 0..<num_list.count {
-        if i % n == 0 {
-            result.append(num_list[i])
-        }
-    }
-    
-    return result
+    num_list.enumerated().filter { $0.offset % n == 0 }.map { $0.element }
 }
