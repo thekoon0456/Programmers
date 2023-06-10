bd17bcadc59e3ac20098841c7ef5325d5703ea64
thekoon <thekoon0456@gmail.com>
2023-06-10 17:20:10 +0900

    [unrated] Title: A 강조하기, Time: 0.05 ms, Memory: 16 MB -BaekjoonHub

--- 프로그래머스/unrated/181874. A 강조하기/A 강조하기.swift
+++ 프로그래머스/unrated/181874. A 강조하기/A 강조하기.swift
@@ -1,5 +1,5 @@
 import Foundation
 
 func solution(_ myString:String) -> String {
-    myString.lowercased().map { if $0 == "a" { return $0.uppercased() }; return String($0) }.joined()
+    myString.map { $0 == "a" || $0 == "A" ? $0.uppercased() : $0.lowercased() }.joined()
 }
