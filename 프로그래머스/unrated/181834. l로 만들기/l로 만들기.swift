40abcdb6cc0b68e79040c0e2017972e295d8ddc4
thekoon <thekoon0456@gmail.com>
2023-06-11 12:55:16 +0900

    [unrated] Title: l로 만들기, Time: 10.86 ms, Memory: 16.8 MB -BaekjoonHub

--- 프로그래머스/unrated/181834. l로 만들기/l로 만들기.swift
+++ 프로그래머스/unrated/181834. l로 만들기/l로 만들기.swift
@@ -1,6 +1,5 @@
 import Foundation
 
 func solution(_ myString:String) -> String {
-    let arr = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k"]
-    return myString.map { arr.contains(String($0)) ? "l" : String($0) }.joined()
+    myString.map { String($0) < "l" ? "l" : String($0) }.joined()
 }
