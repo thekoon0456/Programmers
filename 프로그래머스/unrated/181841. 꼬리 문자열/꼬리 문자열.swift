0d78acbed401b3b172de497ee561f29261f677b4
thekoon <thekoon0456@gmail.com>
2023-06-13 14:10:16 +0900

    [unrated] Title: 꼬리 문자열, Time: 0.06 ms, Memory: 16.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181841. 꼬리 문자열/꼬리 문자열.swift
@@ -0,0 +1,13 @@
+import Foundation
+
+func solution(_ str_list:[String], _ ex:String) -> String {
+    var result = ""
+    
+    for str in str_list {
+        if !str.contains(ex) {
+            result += str
+        }
+    }
+    
+    return result
+}
