6adee8b71523129f7fc5635d9cd7652d1106d964
thekoon <thekoon0456@gmail.com>
2023-06-15 15:20:22 +0900

    [unrated] Title: 순서 바꾸기, Time: 0.07 ms, Memory: 16.3 MB -BaekjoonHub

--- 프로그래머스/unrated/181891. 순서 바꾸기/순서 바꾸기.swift
+++ 프로그래머스/unrated/181891. 순서 바꾸기/순서 바꾸기.swift
@@ -1,13 +1,5 @@
 import Foundation
 
 func solution(_ num_list:[Int], _ n:Int) -> [Int] {
-    var result = [Int]()
-    
-    for i in 0..<num_list.count {
-        if i >= n {
-            result.append(num_list[i])
-        }
-    }
-    
-    return result + num_list.prefix(n) 
+    num_list.suffix(num_list.count - n) + num_list.prefix(n) 
 }
