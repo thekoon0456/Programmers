4777d1b0618119ee1115369ebf048e69575b5752
thekoon <thekoon0456@gmail.com>
2023-06-15 23:53:22 +0900

    [unrated] Title: 간단한 식 계산하기, Time: 0.08 ms, Memory: 16.3 MB -BaekjoonHub

--- 프로그래머스/unrated/181865. 간단한 식 계산하기/간단한 식 계산하기.swift
+++ 프로그래머스/unrated/181865. 간단한 식 계산하기/간단한 식 계산하기.swift
@@ -3,19 +3,15 @@ import Foundation
 func solution(_ binomial:String) -> Int {
     let binomialArr = binomial.split(separator: " ")
     let binomialTuple = (Int(binomialArr[0])!, Int(binomialArr[2])!)
-    var result = 0
     
     switch binomialArr[1] {
         case "+":
-        result = binomialTuple.0 + binomialTuple.1
+        return binomialTuple.0 + binomialTuple.1
         case "-":
-        result =  binomialTuple.0 - binomialTuple.1
+        return binomialTuple.0 - binomialTuple.1
         case "*":
-        result =  binomialTuple.0 * binomialTuple.1
+        return binomialTuple.0 * binomialTuple.1
         default:
-        break
+        return 0
     }
-    
-    return result
-    
 }
