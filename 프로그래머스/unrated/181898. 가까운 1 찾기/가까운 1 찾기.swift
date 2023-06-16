478fa94dd52395c250eff764359337bab32d5392
thekoon <thekoon0456@gmail.com>
2023-06-16 13:58:03 +0900

    [unrated] Title: 가까운 1 찾기, Time: 0.05 ms, Memory: 16.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181898. 가까운 1 찾기/가까운 1 찾기.swift
+++ 프로그래머스/unrated/181898. 가까운 1 찾기/가까운 1 찾기.swift
@@ -1,10 +1,5 @@
 import Foundation
 
 func solution(_ arr:[Int], _ idx:Int) -> Int {
-    var result = arr.suffix(arr.count - idx).firstIndex(of: 1)
-    if let result = result {
-        return result
-    }
-    
-    return -1
+    arr[idx...].firstIndex(of: 1) ?? -1
 }
