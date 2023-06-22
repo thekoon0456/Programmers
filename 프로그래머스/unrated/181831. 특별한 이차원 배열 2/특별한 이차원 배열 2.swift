67c13895a9c91f7a0f751e53c8150523a1b2f571
thekoon <thekoon0456@gmail.com>
2023-06-22 13:18:46 +0900

    [unrated] Title: 특별한 이차원 배열 2, Time: 0.01 ms, Memory: 16.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181831. 특별한 이차원 배열 2/특별한 이차원 배열 2.swift
+++ 프로그래머스/unrated/181831. 특별한 이차원 배열 2/특별한 이차원 배열 2.swift
@@ -1,15 +1,11 @@
 import Foundation
 
 func solution(_ arr:[[Int]]) -> Int {
-    var result = arr.count
-    
     for i in 0..<arr.count {
-        for j in 0..<arr.count {
-            if arr[i][j] != arr[j][i] {
-                result -= 1
-            }
+        for j in 0..<arr.count where arr[i][j] != arr[j][i] {
+                return 0
         }
     }
     
-    return result == arr.count ? 1 : 0
+    return 1
 }
