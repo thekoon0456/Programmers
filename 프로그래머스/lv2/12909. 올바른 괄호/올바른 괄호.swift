25a186b940eee25215458a781f2cf15dadd86f7f
thekoon <thekoon0456@gmail.com>
2023-10-04 16:08:38 +0900

    [level 2] Title: 올바른 괄호, Time: 19.75 ms, Memory: 17 MB -BaekjoonHub

--- 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.swift
+++ 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.swift
@@ -2,49 +2,28 @@ import Foundation
 
 func solution(_ s:String) -> Bool {
     var arr = s.map { String($0) }
-    var stackS: [String] = []
-    var stackM: [String] = []
-    var stackL: [String] = []
+    var stack: [String] = []
     var result: Bool = false
     
-    if !["(", "{", "["].contains(arr[0]) {
-        return false
-    } else if ["(", "{", "["].contains(arr.last) {
+    if arr.first == ")" || arr.last == "(" {
         return false
     }
     
     for i in 0..<arr.count {
         switch arr[i] {
         case let a where a == "(":
-            stackS.append(a)
+            stack.append(a)
         case let a where a == ")":
-            if stackS.isEmpty {
+            if stack.isEmpty {
                 result = false
             } else {
-                stackS.removeLast()
-            }
-        case let a where a == "{":
-            stackM.append(a)
-        case let a where a == "}":
-            if stackM.isEmpty {
-                result = false
-            } else {
-                stackM.removeLast()
-            }
-        case let a where a == "[":
-            stackL.append(a)
-        case let a where a == "]":
-            if stackL.isEmpty {
-                result = false
-            } else {
-                stackL.removeLast()
+                stack.removeLast()
             }
         default:
             return false
         }
     }
     
-    result = stackS.isEmpty && stackM.isEmpty && stackL.isEmpty
-    
+    result = stack.isEmpty   
     return result
 }
